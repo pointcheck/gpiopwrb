54dfb9134a8b74837a09490e852a4086af92fc71
Ruslan Zalata <rz@fabmicro.ru>
2026-05-13 01:39:21 +0500

    Implement calling ACPI method when button is pressed resolved in attach

--- README.md
+++ README.md
@@ -1,33 +1,114 @@
-# Kernel Module for monitoring state of Power Button connected to AMD GPIO
+# FreeBSD Kernel Module for monitoring state of Power Button connected to AMD GPIO
 
-On many Hardware-Reduced PCs equipped with AMD's CPUs, mostly modern laptops,
+On many PCs with Hardware-Reduced ACPI equipped with AMD Ryzen CPUs, which are mostly modern laptops,
 Power Button is wired to AMD GPIO controller. FreeBSD has support for AMD GPIO
-since 2018 implemented by amdgpio.ko module (sys/dev/amdgpio/amdgpio.c).
+since 2018 implemented by **amdgpio** module (sys/dev/amdgpio/amdgpio.c).
 Unfortunately, **amdgpio** module does not support interrupts on amd64 platform,
-it's simply not implemented. This **gpiopwrb** module provides a quick and dirty
-hack to allow Power Buttons to work on such PCs by periodically polling state of
-given GPIO pin and raising ACPI event \_SB_.PWRB.
+it's simply not implemented because of lack of INTRNG support in upstream **gpiobus** module.
+Hence, pressing Power Button will never trigger poweroff or sleep event.
+
+This **gpiopwrb** module provides a quick and dirty hack to allow Power Buttons
+to work on such PCs by periodically polling state of given GPIO pin and calling ACPI method
+like \\_SB.GPIO._EVT or \\_SB.PWRB to raise event.
+
+This module was tested on and developped for HP 15e laptop with the following firmware:
+```
+[000h 0000 004h]                   Signature : "FACP"    [Fixed ACPI Description Table (FADT)]
+[004h 0004 004h]                Table Length : 00000114
+[008h 0008 001h]                    Revision : 06
+[009h 0009 001h]                    Checksum : FF
+[00Ah 0010 006h]                      Oem ID : "HPQOEM"
+[010h 0016 008h]                Oem Table ID : "SLIC-MPC"
+[018h 0024 004h]                Oem Revision : 01072009
+[01Ch 0028 004h]             Asl Compiler ID : "HP  "
+[020h 0032 004h]       Asl Compiler Revision : 00010013
+```
 
 ## How to Build and Install
 
 ```
-sudo make clean
-sudo make
-sudo make install
+make clean
+make
+sudo cp gpiopwrb.ko /boot/kernel/ 
 ```
 
 ## Configuring gpiopwrb module
 
-Simply add below to your **/boot/loader.conf** file:
+1. Add below to your **/boot/loader.conf** file:
 
 ```
-hint.gpiopwrb.pin_num=0 # <-- set GPIO pin number on your machine, default is zero.
-gpiopwrb_load="YES"
+hint.gpiopwrb.0.pin_num=0 # <-- set GPIO pin number on your machine, default is zero.
+hint.gpiopwrb.0.acpi_path="\\_SB.GPIO._EVT" # <-- normally GPIO should trigger _EVT ACPI event
+# or
+hint.gpiopwrb.0.acpi_path="\\_SB.PWRB.PKG2" # <--- on some PCs _EVT is not supported, call to PWRB instead.
 ```
 
-## Documentation to read
+Preserve the double backslash!!!
+
+2. Add **gpiopwrb** module to **kld_list** variable in your /etc/rc.conf:
+
+```
+# sysrc kld_list+=gpiopwrb
+```
+
+3. Reboot.
+
+Notes:
+
+ - Loading **gpiopwrb** module from /boot/loader.conf will not work because it depends on a number of other
+modules which are not provided during boot time.
+
+ - By default, when no hints provided, **gpiopwrb** module uses pin_num = 0 and calls \\_SB.GPIO._EVT ACPI method to trigger event.
+
+ - For testing, you can use kenv(1) command to temporarily set different hints for **gpiopwrb** module:
+
+ - The exact behaviour of your system upon poweroff/sleep event is regulated by **hw.acpi.power_button_state** sysctl variable. 
+
+```
+# kenv hint.gpiopwrb.0.pin_num=0
+# kenv hint.gpiopwrb.0.acpi_path='\_SB.GPIO._EVT'
+```
+
+Use single backslash and single quote character while in shell!
+
+Use ```kldunload gpiopwrb || kldload gpiopwrb``` to reload module with new hints.
+
+## More reads 
+
+A general description of ACPI subsystem can be found in following system man pages:
 
 ```
 man 4 gpiobus
 man 4 acpi
+man 8 iasl
 ```
+
+In some rare cases you might be needing to decompile DSDT table/firmware provided by BIOS on your machine.
+You can easily do that with acpidumpt(8) and iasl(8) tools provided by **acpica-tools** package.
+Basically what you need to know is how Power Button is wired (what GPIO is used) and what ACPI method should
+be called to raise power button event. Look for PWRB method in DSDT and in other AML tables.
+
+## Debugging
+
+1. There's also **sysutils/acpi_call** port that provides kernel module and tool that allows sending
+arbitrary ACPI calls and view returning results, it can be useful for debugging ACPI issues.
+To call \\_SB.PWRB.PKG2 event with acpi_call, use the following syntax:
+
+```
+# acpi_call -v -p '\_SB.GPIO._EVT' -i 0 -i 0x80
+```
+or
+```
+# acpi_call -v -p '\_SB.PWRB.PKG2' -i 0 -i 0x80
+```
+
+Note: if that works, your system will go immediate poweroff/sleep after issuing above commands.
+
+2. It's possible to build **gpiopwrb** module with quite verbose debugging output. For that, define GPIOPWRB_DEBUG macro in source file gpiopwrb.c, recompile and reinstall.
+
+## Links
+
+1. https://en.wikipedia.org/wiki/ACPI
+2. https://wiki.freebsd.org/GPIO
+
+
--- gpiopwrb.c
+++ gpiopwrb.c
@@ -2,7 +2,7 @@
 * pwrb.c - kernel module for Power Button state change
 *
 * It monitors given pin on GPIO port using standard gpio framework.
-* Once state is changed, an PWRB event is raised to ACPI subsystem.
+* Once state is changed, an _EVT or PWRB event is raised to ACPI subsystem.
 *
 */
 #include <sys/param.h>
@@ -23,10 +23,12 @@
 
 #include "gpiobus_if.h"
 
-#define	GPIO_POLL_TIMEO	(1000/4)	// Poll GPIO every 1/4 second
-#define	GPIO_PWRB_PIN_NUM	0	// Pin number to monitor: pin 0
+#define	GPIO_POLL_TIMEO		(1000/4)	// Poll GPIO every 1/4 second
+#define	GPIO_PWRB_PIN_NUM	0		// Pin number to monitor: pin 0
+#define	ACPI_PATH_SIZE		32
+#define	GPIO_PWRB_ACPI_PATH	"\\_SB.GPIO._EVT"	// Default ACPI path to call
 
-//#define	GPIOPWRB_DEBUG	1
+#define	GPIOPWRB_DEBUG	1
 
 #ifdef GPIOPWRB_DEBUG
 #define dprintf printf
@@ -43,9 +45,41 @@ struct gpiopwrb_softc {
 	struct mtx thread_mtx;
 	int thread_terminate;
 	int pin_num;
+	char acpi_path[ACPI_PATH_SIZE];
 } gpiopwrb_softc_sc; /* we use global softc because we need it in MOD_LOAD */
 
 static struct thread *gpiopwrb_daemon_thread;
+
+
+static void gpiopwrb_call_acpi(struct gpiopwrb_softc* sc)
+{
+	ACPI_STATUS status;
+	ACPI_OBJECT args[2];
+	ACPI_OBJECT_LIST arglist;
+
+	memset(args, 0, sizeof(args));
+	memset(&arglist, 0, sizeof(arglist));
+	
+	/* ACPI event \_SB.GPIO._EVT usually takes two arguments:
+	 * arg0 - event source number (GPIO pin number)
+	 * arg1 - 0x80 for status change
+	 *
+	 * ACPI event \_SB.PWRB.PKG2 takes two args that are returned as package
+	 */
+
+	args[0].Type = ACPI_TYPE_INTEGER;
+	args[0].Integer.Value = (ACPI_INTEGER) (INT64)0;	// event source number
+	args[1].Type = ACPI_TYPE_INTEGER;
+	args[1].Integer.Value = (ACPI_INTEGER) (INT64)0x80;	// pin status change
+	arglist.Count = 2;
+	arglist.Pointer = args;
+
+	status = AcpiEvaluateObject(sc->acpi_handle, NULL, &arglist, NULL);
+
+	if(ACPI_FAILURE(status))
+		printf("pwrb: failed to call AcpiEvaluateObject: %s\n",
+			AcpiFormatException(status));
+}
 
 
 static void gpiopwrb_daemon(void* arg)
@@ -86,12 +120,13 @@ static void gpiopwrb_daemon(void* arg)
 
 		bool changed = (gpio_state != gpio_state_prev);
 
-		dprintf("pwrb: poll... gpio pin %d state = %d, changed = %s\n",
-			sc->pin_num, gpio_state, changed ? "YES" : "no");
+		//dprintf("pwrb: poll... gpio pin %d state = %d, changed = %s\n",
+		//	sc->pin_num, gpio_state, changed ? "YES" : "no");
 
 		if (changed) {
-			printf("pwrb: GPIO pin %d changed, emmiting ACPI event\n",
-				sc->pin_num);
+			printf("pwrb: GPIO pin %d changed, emmiting ACPI event %s\n",
+				sc->pin_num, sc->acpi_path);
+			gpiopwrb_call_acpi(sc);
 		}
 
 		gpio_state_prev = gpio_state;
@@ -128,28 +163,46 @@ static void gpiopwrb_identify(driver_t *driver, device_t parent)
 	/* Fetch GPIO pin number from Hints */
 
 	char hint_str[32];
-	char *hint_pin_num;
+	char *hint_value;
 
 	snprintf(hint_str, 32, "hint.%s.%d.pin_num",
 	    device_get_name(sc->child_dev),
 	    device_get_unit(sc->child_dev));
 
-	if ((hint_pin_num = kern_getenv(hint_str)) != NULL)
-		sc->pin_num = strtol(hint_pin_num, NULL, 0);
+	if ((hint_value = kern_getenv(hint_str)) != NULL)
+		sc->pin_num = strtol(hint_value, NULL, 0);
 	else
 		sc->pin_num = GPIO_PWRB_PIN_NUM;
 
-	device_set_softc(sc->child_dev, sc);
-
 	dprintf("pwrb: new child_dev = %p for parrent = %p, pin_num = %d, %s = %s\n",
-		sc->child_dev, parent, sc->pin_num, hint_str, hint_pin_num);
+		sc->child_dev, parent, sc->pin_num, hint_str, hint_value);
+
+	/* Fetch ACPI path from Hints */
+
+	snprintf(hint_str, 32, "hint.%s.%d.acpi_path",
+	    device_get_name(sc->child_dev),
+	    device_get_unit(sc->child_dev));
+
+	if ((hint_value = kern_getenv(hint_str)) != NULL)
+		strncpy(sc->acpi_path, hint_value, ACPI_PATH_SIZE); 
+	else
+		snprintf(sc->acpi_path, ACPI_PATH_SIZE, GPIO_PWRB_ACPI_PATH);
+
+	dprintf("pwrb: acpi path hint = %s, value = %s\n", hint_str, sc->acpi_path);
+
+	device_set_softc(sc->child_dev, sc);
 }
 
 static int gpiopwrb_probe(device_t dev)
 {
+	if (acpi_disabled("gpiopwrb")) {
+		dprintf("pwrb: ACPI is disabled\n");
+		return (ENXIO);
+	}
+
 	device_set_desc(dev, "GPIO Power Button Monitor");
 
-	dprintf("pwrb: probed dev = %p\n", dev);
+	dprintf("pwrb: probed\n");
 
 	return (BUS_PROBE_DEFAULT);
 }
@@ -161,8 +214,13 @@ static int gpiopwrb_attach(device_t dev)
 	struct gpiopwrb_softc *sc = device_get_softc(dev);
 
 	sc->dev = dev;
-	sc->acpi_handle = acpi_get_handle(dev);
 	sc->gpiobus_dev = device_get_parent(dev);
+
+	ACPI_STATUS acpi_status = AcpiGetHandle(NULL, sc->acpi_path, &sc->acpi_handle);
+	if(ACPI_FAILURE(acpi_status)) {
+		printf("pwrb: failed to get ACPI handle for %s: %s\n", sc->acpi_path, AcpiFormatException(acpi_status));
+		return (EINVAL);
+	}
 
 	dprintf("pwrb: gpiobus = %s, child = %s\n",
 		device_get_name(sc->gpiobus_dev), device_get_name(sc->child_dev));
@@ -186,7 +244,7 @@ static int gpiopwrb_attach(device_t dev)
 
 	mtx_init(&sc->thread_mtx, "pwrb thread mtx", NULL, MTX_DEF);
 
-     	error = kthread_add(gpiopwrb_daemon, sc, NULL, &gpiopwrb_daemon_thread,
+	error = kthread_add(gpiopwrb_daemon, sc, NULL, &gpiopwrb_daemon_thread,
 		0, 0, "pwrb_daemon");
 
 	if (error) {
@@ -230,8 +288,6 @@ static int gpiopwrb_detach(device_t dev)
 	return (0);
 }
 
-static devclass_t gpiopwrb_devclass;
-
 static device_method_t gpiopwrb_methods[] = {
 	DEVMETHOD(device_identify, gpiopwrb_identify),
 	DEVMETHOD(device_probe, gpiopwrb_probe),
@@ -241,7 +297,8 @@ static device_method_t gpiopwrb_methods[] = {
 };
 
 DEFINE_CLASS_0(gpiopwrb, gpiopwrb_driver, gpiopwrb_methods, sizeof(struct gpiopwrb_softc));
-DRIVER_MODULE(gpiopwrb, gpiobus, gpiopwrb_driver, gpiopwrb_devclass, 0, 0);	
+DRIVER_MODULE(gpiopwrb, gpiobus, gpiopwrb_driver, 0, 0);	
+MODULE_DEPEND(gpiopwrb, acpi, 1, 1, 1);
 MODULE_DEPEND(gpiopwrb, gpiobus, 1, 1, 1);
 MODULE_DEPEND(gpiopwrb, amdgpio, 1, 1, 1);
 MODULE_VERSION(gpiopwrb, 1);
